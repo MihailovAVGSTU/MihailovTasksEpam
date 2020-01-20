6de8361a851fc66fa576af3d3082f4d670803912
ArtemMihailov <lavash.karica@mail.ru>
2020-01-20 19:53:35 +0300

    При выборе подпунктов из третьего корневого пункта меню осуществляется загрузка одной из трёх дополнительных html-страниц в центральную область окна

--- task2/web/js/script.js
+++ task2/web/js/script.js
@@ -11,3 +11,40 @@ function SubMenu(num){
     document.getElementById("content").innerHTML = "submenu " + num;
 }
 
+function showContent(link) {
+
+    var cont = document.getElementById('content');
+
+    var http = createRequestObject();
+    if( http )
+    {
+        http.open('get', link);
+        http.onreadystatechange = function ()
+        {
+            if(http.readyState == 4)
+            {
+                cont.innerHTML = http.responseText;
+            }
+        }
+        http.send(null);
+    }
+    else
+    {
+        document.location = link;
+    }
+}
+
+// создание ajax объекта
+function createRequestObject()
+{
+    try { return new XMLHttpRequest() }
+    catch(e)
+    {
+        try { return new ActiveXObject('Msxml2.XMLHTTP') }
+        catch(e)
+        {
+            try { return new ActiveXObject('Microsoft.XMLHTTP') }
+            catch(e) { return null; }
+        }
+    }
+}
